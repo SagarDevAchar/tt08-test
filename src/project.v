83d3e15b18c63749c82b58ae244e0c3b97643436
Seemebadnekai <51400137+SagarDevAchar@users.noreply.github.com>
2024-08-04 00:16:52 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -46,6 +46,6 @@ module tt_um_example (
     assign uio_oe  = 8'b1000_0000;
         
     // List all unused inputs to prevent warnings
-    wire _unused = &{ena, 1'b0};
+    wire _unused = &{ena, ui_in, 1'b0};
     
 endmodule
