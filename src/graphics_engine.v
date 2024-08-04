2163b75f4796d960c8983330c801c5ce8f61986c
Seemebadnekai <51400137+SagarDevAchar@users.noreply.github.com>
2024-08-04 08:02:26 +0530

    Update graphics_engine.v

--- src/graphics_engine.v
+++ src/graphics_engine.v
@@ -8,5 +8,5 @@ module graphics_engine(
     assign g = y[9:8] & {2{frame_active}};
     assign b = x[9:8] & {2{frame_active}};
     
-    
+    wire _unused = &{x[7:0], y[7:0], rst_n, 1'b0};
 endmodule
